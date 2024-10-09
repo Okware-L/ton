cb531d12c4b90c21da385db0bc2f8c7f6718dd89
Okware-L <lewisokware@gmail.com>
2024-10-10 00:22:48 +0300

    ...1st iteration done

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,85 @@
 import "./App.css";
 import { TonConnectButton } from "@tonconnect/ui-react";
+import { useMainContract } from "./hooks/useMainContract";
+import { useTonConnect } from "./hooks/useTonConnect";
+import { fromNano } from "@ton/core";
+import WebApp from "@twa-dev/sdk";
 
 function App() {
+  const {
+    contract_address,
+    counter_value,
+    recent_sender,
+    owner_address,
+    contract_balance,
+    sendIncrement,
+    sendDeposit,
+    sendWithdrawalRequest,
+  } = useMainContract();
+
+  const { connected } = useTonConnect();
+
+  const showAlert = () => {
+    WebApp.showAlert("Hey there!");
+  };
+
   return (
     <div>
-      <TonConnectButton />
+      <div>
+        <TonConnectButton />
+      </div>
+      <div>
+        <div className="Card">
+          <b>{WebApp.platform}</b>
+          <b>Our contract Address</b>
+          <div className="Hint">{contract_address?.slice(0, 30) + "..."}</div>
+          <b>Our contract Balance</b>
+          {contract_balance && (
+            <div className="Hint">{fromNano(contract_balance)}</div>
+          )}
+        </div>
+
+        <div className="Card">
+          <b>Counter Value</b>
+          <div>{counter_value ?? "Loading..."}</div>
+        </div>
+
+        <br />
+
+        {connected && (
+          <a
+            onClick={() => {
+              sendIncrement();
+            }}
+          >
+            Increment by 5
+          </a>
+        )}
+
+        <br />
+
+        {connected && (
+          <a
+            onClick={() => {
+              sendDeposit();
+            }}
+          >
+            Request deposit of 1 TON
+          </a>
+        )}
+
+        <br />
+
+        {connected && (
+          <a
+            onClick={() => {
+              sendWithdrawalRequest();
+            }}
+          >
+            Request 0.5 TON withdrawal
+          </a>
+        )}
+      </div>
     </div>
   );
 }
